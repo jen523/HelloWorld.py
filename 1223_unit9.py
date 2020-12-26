5af0900dea095e0cc6f82d152ada4fd4f54f1941
jen523 <76270524+jen523@users.noreply.github.com>
2020-12-26 21:25:00 +0900

    Add files via upload

---
+++ 1223_unit9.py
@@ -0,0 +1,5 @@
+single_quote = '''"안녕하세요."'파이썬'입니다.'''
+print(single_quote)
+'python isn\'t difficult'
+print('Hello\nPython')
+print('hello world')
---
+++ 20201223.py
@@ -0,0 +1,77 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 1>3
+False
+>>> 1 == 1.0
+True
+>>> 1 is 1.0
+<stdin>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+False
+>>> id(1)
+1679109155120
+>>> id(1.0)
+1679115930480
+>>> 1 == 1.0
+True
+>>> a=-5
+>>> a is -5
+<stdin>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+True
+>>> a=-6
+>>> a is -6
+<stdin>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+False
+>>> a == -6
+True
+>>> Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+  File "<stdin>", line 1
+    Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+           ^
+SyntaxError: invalid syntax
+>>> Type "help", "copyright", "credits" or "license" for more information.
+  File "<stdin>", line 1
+    Type "help", "copyright", "credits" or "license" for more information.
+         ^
+SyntaxError: invalid syntax
+>>> >>> 1>3
+  File "<stdin>", line 1
+    >>> 1>3
+    ^
+SyntaxError: invalid syntax
+>>> False
+False
+>>> >>> 1 == 1.0
+  File "<stdin>", line 1
+    >>> 1 == 1.0
+    ^
+SyntaxError: invalid syntax
+>>> True
+True
+>>> >>> 1 is 1.0
+  File "<stdin>", line 1
+    >>> 1 is 1.0
+    ^
+SyntaxError: invalid syntax
+>>> <stdin>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+  File "<stdin>", line 1
+    <stdin>:1: SyntaxWarning: "is" with a literal. Did you mean "=="?
+    ^
+SyntaxError: invalid syntax
+>>> False
+False
+>>> >>> id(1)
+  File "<stdin>", line 1
+    >>> id(1)
+    ^
+SyntaxError: invalid syntax
+>>> 1679109155120
+1679109155120
+>>> >>> id(1.0)
+  File "<stdin>", line 1
+    >>> id(1.0)
+    ^
+SyntaxError: invalid syntax
+>>> 1679115930480
+1679115930480
+>>>
+
